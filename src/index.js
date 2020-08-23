d9b0b7e0c85942eec2d53bd1c18339aea88feb68
Alex Partida <60553154+alpartida@users.noreply.github.com>
2020-08-23 10:47:28 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let country = "Guinea Bissau";
+let challengeOne = country.replace("-", " ");
+console.log(challengeOne);
+let city = " Sydney   ";
+let challengeTwo = city.trim();
+console.log(challengeTwo);
+
+let attraction = "Opera House";
+let challengeThree = attraction.toLowerCase();
+console.log(challengeThree);
+
+let place = "School";
+let challengeFour = place.toUpperCase();
+console.log(challengeFour);
+
+let sentence = `I went to visit the ${attraction} in ${city}, right next to my ${place}. Now, I'm getting ready for my trip to ${country}!`;
+console.log(sentence);
